7dbcbcbcec2c6ef45b17b33700de07aaf35e95ab
KhunVong <KhunVong609@gmail.com>
2025-01-08 16:49:40 +0700

    Vong committed

--- LAB5_COLLABCHEAPANHARITHANDKUNSOTRAVONG/src/Lab5.java
+++ LAB5_COLLABCHEAPANHARITHANDKUNSOTRAVONG/src/Lab5.java
@@ -1,19 +1,28 @@
 // LAB 05 - SE GROUP
 // Name : course in year 2 program
-// Date : 08/1/2025
-// Author : Chea panharith
-// Modified : 08/1/2025
-// Description : This class represents a course subject for Year 2 students, with attributes for name, ID, and score
+// Date : 08/01/2025
+// Author : Chea Panharith, Khun Sotravong
+// Modified : 08/01/2025
+// Description : This class represents a course subject for Year 2 students, with attributes for name, ID, and score.
+//               THis class represents study sessions in a day with attributes for startTime and endTime.
+//              Includes methods for retrieving and validating attributes.
 // Functions: - Constructor: Initializes name, ID, and score.
 //            - getName(): Returns the subject name.
 //            - getId(): Returns the subject ID.
 //            - getScore(): Returns the subject score.
-//            - getMaxScore(): give the max score.
-//            - Method
-//            - Setname() : change the name
-// Variables: - name: The name of the subject (final, private).
-//            - id: The ID of the subject (final, private).
-//            - score: The score of the subject (final, private).
+//            - setName(): Validates and updates the subject name.
+//            - getMaxScore(): Returns the maximum score constant.
+//            - Session Constructor: Initializes startTime and endTime for a study session.
+//            - setDuration(): Validates and sets the start and end times for a session.
+//            - getStartTime(): Returns the session's start time.
+//            - getEndTime(): Returns the session's end time.
+//            - toString(): Provides a string representation of the session.
+// Variables: - name: The name of the subject (private).
+//            - id: The ID of the subject (private).
+//            - score: The score of the subject (private).
+//            - MAX_SCORE: The maximum possible score (static final, private).
+//            - Session.startTime: The start time of the session (private).
+//            - Session.endTime: The end time of the session (private).
 
 class Subject{
     private String name;
@@ -52,6 +61,63 @@ class Subject{
         this.name = name;
     }
 }
+class Session {
+    private int startTime; 
+    private int endTime;   
+
+    /**
+     * Constructor to initialize a study session.
+     *
+     * @param startTime 
+     * @param endTime   
+     * @throws IllegalArgumentException 
+     */
+    public Session(int startTime, int endTime) {
+        setDuration(startTime, endTime);
+    }
+
+    /**
+     * Sets the duration of the study session.
+     *
+     * @param startTime 
+     * @param endTime   
+     * @throws IllegalArgumentException 
+     */
+    public void setDuration(int startTime, int endTime) {
+        if (startTime >= endTime) {
+            throw new IllegalArgumentException("Start time must be less than end time.");
+        }
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    /**
+     * 
+     *
+     * @return The start time.
+     */
+    public int getStartTime() {
+        return startTime;
+    }
+
+    /**
+     * 
+     *
+     * @return The end time.
+     */
+    public int getEndTime() {
+        return endTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Session{" +
+                "startTime=" + startTime +
+                ", endTime=" + endTime +
+                '}';
+    }
+}
+
 
 public class Lab5{
     @SuppressWarnings("static-access")
@@ -61,6 +127,6 @@ public class Lab5{
         System.out.println(subject.getId());
         System.out.println(subject.getScore());
         System.out.println(subject.getMaxScore());
-        
+
     }
 }
